9ef1497f797e85e488db33a75b7c0b6181470695
girl-interrupted <d.pavliuchyk@gmail.com>
2021-02-24 17:44:15 +0000

    temp reset, usage definition and html file

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -13,7 +13,7 @@ describe('Thermostat', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20)
   });
 
-  it('uncreases in temperature with up()', function() {
+  it('increases in temperature with up()', function() {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21)
   });
@@ -39,12 +39,64 @@ describe('Thermostat', function() {
     expect(thermostat.isPowerSavingModeOn()).toBe(false);
   });
 
+  it('can switch power saving mode back on', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
 
-it('can switch power saving mode back on', function() {
-  thermostat.switchPowerSavingModeOff();
-  expect(thermostat.isPowerSavingModeOn()).toBe(false);
-  thermostat.switchPowerSavingModeOn();
-  expect(thermostat.isPowerSavingModeOn()).toBe(true);
-});
+  describe('when power saving mode is on', function() {
+    it('has a max temperature of 25 degrees', function() {
+      for (var count = 0; count < 6; count++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
 
+  describe('when power saving mode is off', function() {
+    it('has a max temperature of 32 degrees', function() {
+      thermostat.switchPowerSavingModeOff();
+      for (var count = 0; count < 13; count++) {
+        thermostat.up()
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
+
+  it('can be reset to the default temperature', function() {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    thermostat.resetTemp();
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  describe('displaying usage levels', function() {
+    describe('when the temperature is below 18 degrees', function() {
+      it('is considered low-usage', function() {
+        for (var count = 0; count < 3; count++) {
+          thermostat.down();
+        }
+        expect(thermostat.energyUsage()).toEqual('low-usage');
+      });
+    });
+
+    describe('when the temperature is between 18 and 25 degrees', function() {
+      it('is considered medium-usage', function() {
+        expect(thermostat.energyUsage()).toEqual('medium-usage');
+      });
+    });
+
+    describe('when the temperature is anything else', function() {
+      it('is considered high-usage', function() {
+        thermostat.powerSavingMode = false;
+        for (var count = 0; count < 6; count++) {
+          thermostat.up();
+        }
+        expect(thermostat.energyUsage()).toEqual('high-usage');
+      });
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,31 +3,67 @@
 class Thermostat{
   constructor() {
     this.MIN_TEMPERATURE = 10;
-    this.temperature = 20;
+    this.DEFAULT_TEMP = 20;
+    this.temperature = this.DEFAULT_TEMP;
     this.powerSavingMode = true;
+    this.PSM_ON_MAX_LIMIT = 25;
+    this.PSM_OFF_MAX_LIMIT = 32;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
   }
+
   getCurrentTemperature() {
     return this.temperature;
   }
+
   up() {
+    if (this.isMaxTemperature()) {
+      return;
+    }
     this.temperature +=1
   }
+
   down() {
     if (this.isMinimumTemperature()) {
       return;
     }
     this.temperature -=1
   }
+
   isMinimumTemperature() {
     return this.temperature === this.MIN_TEMPERATURE;
   }
+
   isPowerSavingModeOn() {
     return this.powerSavingMode === true;
   }
+
   switchPowerSavingModeOff() {
     return this.powerSavingMode = false;
   }
+
   switchPowerSavingModeOn() {
     return this.powerSavingMode = true;
+  }
+
+  isMaxTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.PSM_OFF_MAX_LIMIT;
+    }
+    return this.temperature === this.PSM_ON_MAX_LIMIT;
+  }
+
+  resetTemp(){
+    this.temperature = this.DEFAULT_TEMP;
+  }
+
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
   }
 };
